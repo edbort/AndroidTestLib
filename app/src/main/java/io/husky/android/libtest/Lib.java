5f3f64fed39c65e2eaafa791d01750df519d9a44
Edison Bortolin <edbort>
2020-03-04 14:34:50 -0300

    Removido propriedades conflitantes

--- app/src/main/java/io/husky/android/libtest/Lib.java
+++ app/src/main/java/io/husky/android/libtest/Lib.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 
 import io.husky.android.libtest.activity.AccessActivity;
 
-public class Lib {
+public class Lib extends CordovaPlugin {
 
     public static void criaConta(Context context, String jsonArgs, String callback) {
         Intent i = new Intent(context, AccessActivity.class);
