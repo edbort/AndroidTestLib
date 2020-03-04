d292ee129a882fdd6977e5efcb48df0ed7d9cd0f
Edison Bortolin <edbort>
2020-03-04 14:40:02 -0300

    Removido dependencia do Cordova

--- app/src/main/java/io/husky/android/libtest/Lib.java
+++ app/src/main/java/io/husky/android/libtest/Lib.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 
 import io.husky.android.libtest.activity.AccessActivity;
 
-public class Lib extends CordovaPlugin {
+public class Lib {
 
     public static void criaConta(Context context, String jsonArgs, String callback) {
         Intent i = new Intent(context, AccessActivity.class);
